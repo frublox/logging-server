12ea9d1d32ea1fc3149595b7508758ca07e74643
frublox <darkeneddan@gmail.com>
2015-10-17 12:29:41 -0400

    Fixed LogAPI endpoint issue.

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,11 @@
 module Main where
 
 -- import Database.SQLite.Simple
+import System.IO
+import GHC.Generics
 
 import Data.Aeson
 import Data.Time.Calendar
-
-import GHC.Generics
 
 import Network.Wai
 import Network.Wai.Handler.Warp
@@ -18,27 +18,42 @@ import Network.Wai.Handler.Warp
 import Servant
 
 import Control.Monad.Trans.Either
-
-type Coord = Double
-type Longitude = Coord
-type Latitude = Coord
+import Control.Monad.IO.Class
 
 data LogInfo = LogInfo {
-	appId :: String,
-	message :: String,
-	messageType :: String,
-	location :: String,
-	uuid :: Integer,
-	platform :: String,
-	model :: String
+    appName :: String,
+    message :: String,
+    messageType :: String,
+    location :: String,
+    uuid :: Integer,
+    platform :: String,
+    model :: String
 } deriving (Show, Generic)
 
 instance FromJSON LogInfo
+instance ToJSON LogInfo
 
-type LogAPI = "log" :> ReqBody '[JSON] LogInfo :> Post '[JSON] LogInfo
+type LogAPI = 
+    "log" :> ReqBody '[JSON] LogInfo :> Post '[JSON] ()
+
+formatLog :: LogInfo -> String
+formatLog logInfo =
+    appName logInfo ++ 
+    " | " ++ show (uuid logInfo) ++ 
+    " | " ++ location logInfo ++
+    " | " ++ platform logInfo ++ " - " ++ model logInfo ++ 
+    " | " ++ messageType logInfo ++ ": " ++ message logInfo
+
+logToFile :: LogInfo -> IO ()
+logToFile logInfo = do
+    withFile "log.txt" AppendMode
+        (\fileHandle -> do
+            let entry = formatLog logInfo
+            hPutStrLn fileHandle entry)
 
 doLog :: LogInfo -> EitherT ServantErr IO ()
-doLog logInfo = print logInfo
+doLog logInfo = do
+    liftIO $ logToFile logInfo
 
 server :: Server LogAPI
 server = doLog
