305205c38d5cd581d562ad401009c07e5f372134
frublox <darkeneddan@gmail.com>
2015-10-24 13:29:31 -0400

    More work on FromFormUrlEncoded instance.

--- src/Main.hs
+++ src/Main.hs
@@ -12,35 +12,32 @@ import GHC.Generics
 
 import Data.Aeson
 import Data.Time.Calendar
+import Servant
 
 import Network.Wai
 import Network.Wai.Middleware.Cors
 import Network.Wai.Handler.Warp
 
-import Servant
-
 import Control.Monad.Trans.Either
 import Control.Monad.IO.Class
 
-data LogInfo = LogInfo {
-    appName :: String,
-    message :: String,
-    messageType :: String,
-    location :: String,
-    uuid :: Integer,
-    platform :: String,
-    model :: String
-} deriving (Show, Generic)
+import Data.Text (Text)
+import qualified Data.Text as Text 
+import qualified Data.Text.IO as Text
+import Data.Monoid ((<>))
 
-instance FromJSON LogInfo
-instance ToJSON LogInfo
+import Data.LogInfo
+import Format.Pretty
+import Utils.Text (showText)
 
-type LogAPI = "log" :> ReqBody '[JSON] LogInfo :> Post '[JSON] ()
+type LogAPI = 
+    "log" :> ReqBody '[FormUrlEncoded] LogInfo 
+          :> Post '[FormUrlEncoded] ()
 
 main :: IO ()
 main = do
     let port = 3000
-    putStrLn ("Running logging server on port " ++ show port)
+    Text.putStrLn ("Running on port " <> showText port)
     run port app
 
 -- We're using simpleCors to get around the cross-domain policy
@@ -55,20 +52,11 @@ server :: Server LogAPI
 server = doLog
 
 doLog :: LogInfo -> EitherT ServantErr IO ()
-doLog logInfo = liftIO (logToFile logInfo)
+doLog logInfo = liftIO $ do
+    Text.putStrLn (prettify logInfo)
+    logToFile logInfo
 
 logToFile :: LogInfo -> IO ()
 logToFile logInfo = do
-    withFile "log.txt" AppendMode
-        (\fileHandle -> do
-            let entry = formatLog logInfo
-            putStrLn entry
-            hPutStrLn fileHandle entry)
-
-formatLog :: LogInfo -> String
-formatLog logInfo =
-    appName logInfo ++ 
-    " | " ++ show (uuid logInfo) ++ 
-    " | " ++ location logInfo ++
-    " | " ++ platform logInfo ++ " - " ++ model logInfo ++ 
-    " | " ++ messageType logInfo ++ ": " ++ message logInfo
+    let entry = prettify logInfo
+    Text.writeFile "log.txt" entry
